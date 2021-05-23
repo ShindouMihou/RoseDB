9bb2b08552cf337dd3813e4ce44c030a2e668a39
Mihou <69381903+ShindouMihou@users.noreply.github.com>
2021-05-23 18:43:38 +0800

    Improve checking for config & config version checking & update checker.

--- src/main/java/pw/mihou/rosedb/RoseDB.java
+++ src/main/java/pw/mihou/rosedb/RoseDB.java
@@ -1,10 +1,12 @@
 package pw.mihou.rosedb;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import pw.mihou.rosedb.connections.RoseServer;
 import pw.mihou.rosedb.enums.Levels;
 import pw.mihou.rosedb.io.FileHandler;
 import pw.mihou.rosedb.utility.Terminal;
+import pw.mihou.rosedb.utility.UpdateChecker;
 
 import java.io.File;
 import java.net.URISyntaxException;
@@ -19,38 +21,54 @@ public class RoseDB {
     public static int cores = 1;
 
     public static void main(String[] args) throws URISyntaxException {
-        String defaultPath = new StringBuilder(new File(RoseDB.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
-                .getParentFile().getPath()).append(File.separator).append("Database").append(File.separator).toString();
 
+        Terminal.setLoggingLevel(Levels.ERROR);
         if (!new File("config.json").exists()) {
             FileHandler.writeToFile("config.json", new JSONObject()
-                    .put("directory", defaultPath)
-                    .put("port", port).put("authorization", UUID.randomUUID().toString())
-                    .put("loggingLevel", "INFO").put("cores", 1).toString()).join();
+                    .put("directory", new File(RoseDB.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath() + File.separator + "Database" + File.separator)
+                    .put("port", port)
+                    .put("authorization", UUID.randomUUID().toString())
+                    .put("loggingLevel", "INFO")
+                    .put("cores", 1)
+                    .put("updateChecker", true)
+                    .put("configVersion", UpdateChecker.CONFIG_VERSION)
+                    .toString()).join();
         }
 
         JSONObject config = new JSONObject(FileHandler.read("config.json").join());
 
-        port = config.isNull("port") ? 5995 : config.getInt("port");
+        if (config.isNull("configVersion") || !config.getString("configVersion").equals(UpdateChecker.CONFIG_VERSION)) {
+            Terminal.log(Levels.INFO, "We have noticed that your config.json is outdated, we are currently going to perform a short configuration update.");
+            Terminal.log(Levels.INFO, "Don't worry, there isn't anything you need to do on your side!");
+            FileHandler.writeToFile("config.json", updateConfig(config).toString()).join();
+            Terminal.log(Levels.INFO, "Configuration update was complete.");
+        }
 
-        cores = config.isNull("cores") ? 1 : config.getInt("cores");
+        try {
+            port = config.getInt("port");
+            cores = config.getInt("cores");
+            directory = config.getString("directory");
+            authorization = config.getString("authorization");
 
-        directory = Optional.ofNullable(config.getString("directory")).orElse(defaultPath);
+            if (!new File(directory).exists()) {
+                boolean mkdirs = new File(directory).mkdirs();
 
-        authorization = Optional.ofNullable(config.getString("authorization"))
-                .orElse("null");
+                if(!mkdirs){
+                    Terminal.log(Levels.ERROR, "We couldn't create the folders on " + directory);
+                }
+            }
 
-        Terminal.setLoggingLevel(rootLevel(Optional.ofNullable(config.getString("loggingLevel"))
-                .orElse("INFO")));
-
-        if (!new File(directory).exists())
-            new File(directory).mkdirs();
-
-        if (new File(directory).canWrite() && new File(directory).canRead()) {
-            // There is a reason why we don't preload, especially for databases that are very large.
-            RoseServer.run(port);
-        } else {
-            Terminal.log(Levels.ERROR, "Rose cannot write on read or read on " + directory);
+            if (new File(directory).canWrite() && new File(directory).canRead()) {
+                // We are moving original setting of root level here, we want errors to be logged for startup.
+                Terminal.setLoggingLevel(rootLevel(Optional.ofNullable(config.getString("loggingLevel"))
+                        .orElse("INFO")));
+                RoseServer.run(port);
+            } else {
+                Terminal.log(Levels.ERROR, "Rose cannot write on read or read on " + directory);
+            }
+        } catch (JSONException e){
+            Terminal.log(Levels.ERROR, e.getMessage());
+            Terminal.log(Levels.ERROR, "An error from this side is caused by a misconfiguration of config.json, please fix your config.json.");
         }
 
     }
@@ -58,6 +76,17 @@ public class RoseDB {
     public static Levels rootLevel(String configValue) {
         return configValue.equalsIgnoreCase("DEBUG") ? Levels.DEBUG : (configValue.equalsIgnoreCase("INFO") ? Levels.INFO :
                 (configValue.equalsIgnoreCase("ERROR") ? Levels.ERROR : Levels.WARNING));
+    }
+
+    private static JSONObject updateConfig(JSONObject original) throws URISyntaxException, JSONException {
+        return new JSONObject().put("directory", Optional.ofNullable(original.getString("directory"))
+                .orElse(new File(RoseDB.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile().getPath() + File.separator + "Database" + File.separator))
+                .put("port", original.isNull("port") ? 5995 : original.getInt("port"))
+                .put("authorization", Optional.ofNullable(original.getString("authorization")).orElse(UUID.randomUUID().toString()))
+                .put("loggingLevel", Optional.ofNullable(original.getString("loggingLevel")).orElse("INFO"))
+                .put("cores", original.isNull("cores") ? 1 : original.getInt("cores"))
+                .put("updateChecker", original.isNull("updateChecker") ? true : original.getBoolean("updateChecker"))
+                .put("configVersion", UpdateChecker.CONFIG_VERSION);
     }
 
 }
---
+++ src/main/java/pw/mihou/rosedb/utility/UpdateChecker.java
@@ -0,0 +1,36 @@
+package pw.mihou.rosedb.utility;
+
+import pw.mihou.rosedb.enums.Levels;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class UpdateChecker {
+
+    // Config Version is used to validate for configuration changes.
+    // This will not be checked through the online version and will instead be
+    // done on the application itself.
+    public static final String CONFIG_VERSION = "1.1";
+    private static final String ENCODED_VERSION = URLEncoder.encode("1.0.5", StandardCharsets.UTF_8);
+
+    public static boolean check(){
+        try {
+            HttpURLConnection url = (HttpURLConnection) new URL(new StringBuilder("https://cdn.mihou.pw/rosedb/?update=")
+                    .append(ENCODED_VERSION).toString()).openConnection();
+
+            url.setRequestMethod("GET");
+            url.connect();
+
+            return url.getResponseCode() == 200;
+
+        } catch (IOException exception) {
+            Terminal.log(Levels.WARNING,
+                    "Failed to connect to update check endpoint, if you are running offline mode, please disable update checker on config.json");
+        }
+        return false;
+    }
+
+}
